5d4aed6f8ca96188ca7a685606a48df0c3d484d4
Jeas0001 <jeas0001@edu.mercantec.dk>
2025-01-21 14:25:47 +0100

    Better way to switch between rooms

--- BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c050e7f1495649846da151e269a4d581a6ae6e18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1f4b696a5c22023578928ad78b598a36387d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c050e7f1495649846da151e269a4d581a6ae6e18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1f4b696a5c22023578928ad78b598a36387d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
